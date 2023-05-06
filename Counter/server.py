20ece13fed0965ef7e2673790cbd90fe33bff1f1
Jaime Rodriguez <129904062+jrxdriguez@users.noreply.github.com>
2023-05-05 23:26:34 -0700

    Add files via upload

---
+++ Counter/server.py
@@ -0,0 +1,18 @@
+from flask import Flask , render_template, session, redirect # Import Flask to allow us to create our app
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+app.secret_key = 'elsecreto'
+
+@app.route('/')  
+def visits():
+    session['counter'] += 1
+    return render_template('index.html', counter = session['counter'])
+
+@app.route('/destroy_session')
+def destroy():
+    session['counter'] = 0
+    return redirect('/')
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True, port=5001)    # Run the app in debug mode.
